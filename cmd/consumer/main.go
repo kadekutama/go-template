f2f8eb8267aae38c5266d07630712f79705afb4d
go-template agent <go-template-agent@localhost>
2026-09-13 12:24:23 +0700

    feat(E00-T01): initialize Go module and folder structure

---
+++ cmd/consumer/main.go
@@ -0,0 +1,4 @@
+// Command consumer is the NATS consumer entrypoint (E00-T01 stub: no I/O, no ports).
+package main
+
+func main() {}
---
+++ cmd/graphql-api/main.go
@@ -0,0 +1,4 @@
+// Command graphql-api is the GraphQL API entrypoint (E00-T01 stub: no I/O, no ports).
+package main
+
+func main() {}
